518804bf3ce013e458e7f357e3d90ce16daf1f58
parabatareek <gavrosh07@gmail.com>
2023-03-07 22:40:07 +0300

    add: Разработка оболочки - 3.4.4. Настройка приложения для использования Shell

--- js/spa.shell.js
+++ js/spa.shell.js
@@ -11,22 +11,21 @@
 /*global $, spa */
 spa.shell = (function () {
     //#region ПЕРЕМЕННЫЕ В ОБЛАСТИ ВИДИМОСТИ МОДУЛЯ
+    var
     configMap = {
         main_html: String()
-            + '<div id="spa">'
-            + '<div class="spa-shell-head"></div>'
-            + '<div class="spa-shell-head-logo"></div>'
-            + '<div class="spa-shell-head-acct"></div>'
-            + '<div class="spa-shell-head-search"></div>'
+            + '<div class="spa-shell-head">'
+                + '<div class="spa-shell-head-logo"></div>'
+                + '<div class="spa-shell-head-acct"></div>'
+                + '<div class="spa-shell-head-search"></div>'
             + '</div>'
             + '<div class="spa-shell-main">'
-            + '<div class="spa-shell-main-nav"></div>'
-            + '<div class="spa-shell-main-content"></div>'
+                + '<div class="spa-shell-main-nav"></div>'
+                + '<div class="spa-shell-main-content"></div>'
             + '</div>'
-            + '<div class="spa-shell-foot">'
+            + '<div class="spa-shell-foot"></div>'
             + '<div class="spa-shell-chat"></div>'
             + '<div class="spa-shell-modal"></div>'
-            + '</div>'
     },
         stateMap = {$container: null},
         jQueryMap = {},
